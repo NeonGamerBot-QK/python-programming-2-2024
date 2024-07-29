c580f6aa53a8af9413e43b821bb1cfbc5553ca1b
Ford McDonald <ford_mcdonald@brown.edu>
2024-07-29 07:14:59 -0400

    project 4: tree search

---
+++ treesearch/BSTree.py
@@ -0,0 +1,31 @@
+from TreeNode import *
+
+class BSTree:
+    def __init__(self):
+        # YOUR CODE GOES HERE
+        pass
+
+    def insert(self, key, data):
+        # YOUR CODE GOES HERE
+        pass
+
+    def _insert(self, node, key, data):
+        # YOUR CODE GOES HERE
+        pass
+
+
+    def search(self, key):
+        # YOUR CODE GOES HERE
+        pass
+    
+    def _search(self, node, key):
+        # YOUR CODE GOES HERE
+        pass
+    
+    def inorder_traversal(self):
+        # YOUR CODE GOES HERE
+        pass
+    
+    def _inorder_traversal(self, node):
+        # YOUR CODE GOES HERE
+        pass
